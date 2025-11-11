6009b0450ad6c7d8b15e88bc8b42658547a7c831
65SH <shadialhomsi70@gmail.com>
2025-11-11 03:34:24 +0300

    Update isgorusmesi.c

--- Shadi_Alhomsi/isgorusmesi.c
+++ Shadi_Alhomsi/isgorusmesi.c
@@ -1,41 +1,80 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="isgorusmesi" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/isgorusmesi" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/isgorusmesi" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-		</Compiler>
-		<Unit filename="main.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Extensions>
-			<lib_finder disable_auto="1" />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // is icin katilma kurallari
+    int yas, uni_ort;
+    char tecrube[5], diploma[5];
+
+    printf("Sirketimize hosgeldin\n");
+    printf("Yasiniz kac: ");
+
+    while(1){ //Döngü
+    if (scanf("%d", &yas) != 1 ){
+        printf("Lutfen bir sayi giriniz \n");
+        while (getchar() != '\n'); // yanlış girişi temizlemek için yoksa sonsuz döngüye girer
+        continue; //soruyu bir daha sormak için
+    }
+    else if (yas < 18 || yas > 30) { // "||" VEYA anlamına gelir
+        printf("Maalesef yasiniz standartlara uymuyor\n");
+        return 0;  //programı bitir
+        }
+     else{
+        break;
+    }
+}
+    // TECRÜBE SORUSU TEKRAR EDİLİYOR
+    while (1) {
+        printf("Tecrubeniz var mi (evet/hayir): ");
+        scanf("%s", tecrube);
+
+        if (strcmp(tecrube, "hayir") == 0) {
+            printf("Tecrubeli bir elemana ihtiyacimiz var\n");
+            return 0;
+        }
+        else if (strcmp(tecrube, "evet") == 0) {
+            break; // doğru cevap verildi döngüden çık
+        }
+        else {
+            printf("Lutfen 'evet' veya 'hayir' ile cevap verin.\n");
+        }
+    }
+
+    // DİPLOMA SORUSU
+    while (1){
+    printf("Diplomaniz var mi (evet/hayir): ");
+    scanf("%s", diploma);
+
+    if (strcmp(diploma, "hayir") == 0) {
+        printf("En az universite diplomasi lazim\n");
+        return 0;
+    }
+    else if (strcmp(diploma,"evet") == 0){
+        break;
+    }
+    else {
+        printf("Lutfen 'evet' veya 'hayir' ile cevap verin.\n");
+    }
+    }
+    // ORTALAMA SORUSU
+    while(1){
+    printf("Universite ortalamaniz kac: ");
+
+    if (scanf("%d", &uni_ort) != 1){
+    printf("lutfen bir sayi giriniz.\n");
+    while (getchar() != '\n'); // yanlış girişi temizle
+            continue; // yeniden sor
+    }
+
+    else if (uni_ort > 0 && uni_ort < 100) {
+        printf("Tesekkurler, is gorusmesi icin hazirsiniz.\n");
+        break;
+        }
+       else {
+            printf("Lutfen pozitif bir sayi giriniz.\n");
+        }
+    }
+
+    return 0;
+}
